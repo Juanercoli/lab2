6c2368ea775be4f555228f2d75ddc0bee2478e44
Juan Ercoli <ercolijuan@yahoo.com.ar>
2022-09-11 16:38:44 +0000

    fix colon

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,4 @@
 class Monster < ApplicationRecord
     validations :name, presence: true # name not blank validation
-    validations :name, uniqueness { case_sensitive: false }# name uniqueness validation
+    validations :name, uniqueness: { case_sensitive: false }# name uniqueness validation
 end
