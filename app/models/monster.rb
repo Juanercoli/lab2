8b311cd3e1bfa6e131c50ecad16e318f8f0d17cb
Juan Ercoli <ercolijuan@yahoo.com.ar>
2022-09-19 22:55:10 +0000

    Agregada tabla de Tweets con clave foranea

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,5 +3,7 @@ class Monster < ApplicationRecord
     validates :name, uniqueness: { case_sensitive: false } # name uniqueness validation
 
     has_many :attacks, dependent: :destroy
-    has_many :victims, through: :attacks    
+    has_many :victims, through: :attacks 
+       
+    has_many :tweets, dependent: :destroy
 end
