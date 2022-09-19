64655c5fde3001597eca30feb8a152a7f2f001eb
Juan Ercoli <ercolijuan@yahoo.com.ar>
2022-09-19 19:37:32 +0000

    Agregado destruir dependencias

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -2,6 +2,6 @@ class Monster < ApplicationRecord
     validates :name, presence: true # name not blank validation
     validates :name, uniqueness: { case_sensitive: false } # name uniqueness validation
 
-    has_many :attacks
+    has_many :attacks, dependent: :destroy
     has_many :victims, through: :attacks    
 end
--- app/models/victim.rb
+++ app/models/victim.rb
@@ -1,6 +1,6 @@
 class Victim < ApplicationRecord 
     validates :name, presence: true
 
-    has_many :attacks
+    has_many :attacks, dependent: :destroy
     has_many :monsters, through: :attacks    
 end
