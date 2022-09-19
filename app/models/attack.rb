02260d67c6be962c94779f77ec914b710d0e231e
Juan Ercoli <ercolijuan@yahoo.com.ar>
2022-09-19 05:53:57 +0000

    Validaciones check

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -6,7 +6,7 @@ class Attack < ApplicationRecord
 
     def monster_with_scare_level_higher_than_five_cant_have_teen_victims
         if(monster.scare_level > 5 && (2022 - victim.birthdate.year)<18)
-            errors.add(:base, :invalid, message: " #{monster.name} no puede acosar")
+            errors.add(:base, :invalid, message: " #{monster.name} no puede asustar.")
         end 
     end
 end
